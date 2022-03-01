607b529a3cac73ce649c1fb6d673c234b892e365
Shamim <developer.shamimhosen@gmail.com>
2022-03-02 02:58:58 +0600

    forth-Commit

--- js/app.js
+++ js/app.js
@@ -19,12 +19,12 @@ const showPhone = (phone) => {
 div.classList.add('col-lg-4');
 div.classList.add('mb-2');
 div.innerHTML = `
-    <div class="card border-primary m-3 text-center style="width: 14rem;">
-        <img src="${phone.image}" style="width: 16rem;" class="card-img-top img-w-50 mx-auto" alt="...">
+    <div class="card border-primary p-3 border bg-light gy-5 text-center style="width: 14rem;">
+        <img src="${phone.image}" style="width: 14rem;" class="card-img-top img-w-25 mx-auto" alt="...">
     <div class="card-body border-0">
-       <h5 class="card-title">Brand:${phone.brand}</h5>
-        <p class="card-text">Model:${phone.phone_name}</p>
-        <button onclick="moreInfo('${phone.slug}')" 
+       <h5 class="card-title"> <b>Brand:</b> ${phone.brand}</h5>
+        <p class="card-text"><b>Model:</b> ${phone.phone_name}</p>
+        <button onclick="seeDetailsBtn('${phone.slug}')" 
             class="btn btn-danger text-white">Details</button>
       </div>
   </div>
@@ -32,3 +32,39 @@ div.innerHTML = `
     phoneContainer.appendChild(div);
     }
 };
+
+// See Details Button
+const seeDetailsBtn = (slug) => {
+    error.innerHTML ="";
+    const url =`https://openapi.programming-hero.com/api/phone/${slug}`;
+    fetch (url)
+            .then (res => res.json())
+            .then (data => seeDetailsBtn(data.data));
+            console.log(url);
+}
+
+// See Details Result 
+const seeDetailsSection =(allinfo) => {
+    const div = document.createElement('div');
+    div.classList.add('col-12');
+    div.classList.add('mb-2');
+    div.innerHTML =`
+    <div class="card border-info m-3>
+        <img src="${allinfo.image}"style="width-14rem"; 
+             class="card-img-top w-50 mx-auto m-3">
+    <div class=card-body>
+        <h5 class="card-title">${allinfo.name}</h5>
+        <h6 class="card-text"><b>Brand:</b> ${allinfo.brand}</h6>
+        <p class="card-text"><b>Release Date:</b>${allinfo.releaseDate? allinfo.releaseDate: "Sorry, we didn't find the Release date"}</p>
+        <p class="card-text"><b>Sensors:</b>${allinfo.mainFeatures.sensors? allinfo.mainFeatures.sensors: "Sorry, we didn't find the Sensors"}</p>
+        <p class="card-text"><b>Chipset:</b>${allinfo.mainFeatures.chipSet? allinfo.mainFeatures.chipSet: "Sorry, we didn't find the Chip Set"}</p>
+        <p class="card-text"><b>Display Size:</b> ${allinfo.mainFeatures.displaySize}</p>
+        <p class="card-text"><b>Memory:</b> ${allinfo.mainFeatures.memory}</p>
+        <p class="card-text"><b>Display Size:</b> ${allinfo.mainFeatures.storage}</p>
+        
+     </div>
+    </div>
+    `;
+    error.appendChild(div);
+
+}

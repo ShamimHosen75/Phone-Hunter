804561d14aa22d202bf59d8967695f3b71637fc7
Shamim <developer.shamimhosen@gmail.com>
2022-03-02 01:31:32 +0600

    third push

--- js/app.js
+++ js/app.js
@@ -1,30 +1,34 @@
 const searchBrand = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    const url = `https://openapi.programming-hero.com/api/phones?search= ${(searchText)}`;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url) 
         .then (res => res.json())
-        .then ((data) => showPhone(data.data.slice(0,20)));
+        .then (data => showPhone(data.data));
 };
 
-const showPhone = (phones) => {
-    for (const phone of phones){
-     const phoneContainer =document.getElementById("phone-container");
+const showPhone = (phone) => {
+    console.log(phone);
+     const first20Data = phone.slice(0, 20);
+    for (const phone of first20Data){
+        const phoneContainer =document.getElementById("phone-container");
+        const div =document.createElement('div');
+        // console.log(phone.brand);
 
-     const div =document.createElement('div');
-        div.classList.add('col')
-        div.innerHTML = `
-        <div class="card p-3 text-center shadow-lg">
-        <img src="${phone.images}" class="card-img-top w-50 mx-auto" alt="...">
-        <div class="card-body border-0">
-            <h5 class="card-title">Brand:${phone.brand}</h5>
-            <p class="card-text">Model:${phone.phone_name}</p>
-            <button onclick="moreInfo('${phone.slug}')" 
-                class="btn btn-info text-white">Details</button>
-        </div>
-    </div>
-        `;
-    phoneContainer.appendchild(div);
 
+div.classList.add('col-lg-4');
+div.classList.add('mb-2');
+div.innerHTML = `
+    <div class="card border-primary m-3 text-center style="width: 14rem;">
+        <img src="${phone.image}" style="width: 16rem;" class="card-img-top img-w-50 mx-auto" alt="...">
+    <div class="card-body border-0">
+       <h5 class="card-title">Brand:${phone.brand}</h5>
+        <p class="card-text">Model:${phone.phone_name}</p>
+        <button onclick="moreInfo('${phone.slug}')" 
+            class="btn btn-danger text-white">Details</button>
+      </div>
+  </div>
+    `;
+    phoneContainer.appendChild(div);
     }
 };
